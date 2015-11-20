72245afd1df40d980e62b6a15b45365325755659
Jason Olson <jason.s.olson@gmail.com>
2015-11-20 10:30:12 -0600

    Cleaning up server.R code

--- server.r
+++ server.r
@@ -4,8 +4,6 @@ require(e1071);
 require(randomForest);
 require(nnet);
 require(glmnet);
-#install.packages('devtools')
-#devtools::install_github('shiny-incubator', 'rstudio')
 
 set.seed(11111)
 
@@ -17,18 +15,21 @@ inTrain <- createDataPartition(y=fullDataset$Species, p=0.7, list=FALSE)
 trainDataset <- fullDataset[inTrain,]
 testDataset <- fullDataset[-inTrain,]
 
+# Generate model and test the accuracy
 model_lda <- train(Species~., data=trainDataset, method="lda")
 model_lda_Accuracy <- max(model_lda$results$Accuracy)
 predict_lda <- predict(model_lda, testDataset)
 cf_lda <- confusionMatrix(predict_lda, testDataset$Species)
 modelTest_lda_Accuracy <- cf_lda$overall["Accuracy"]
 
+# predict the species type and return the result
 irisPrediction <- function(SLength, SWidth, PLength, PWidth) { 
     inputParams <- data.frame("Sepal.Length" = SLength, "Sepal.Width" = SWidth, "Petal.Length" = PLength, "Petal.Width" = PWidth)
     prediction <- predict(model_lda, inputParams)
     prediction
 }
 
+# get the corresponding image to the predicted type
 getImage <- function(selectedImage) {
     switch(as.character(selectedImage),
         "setosa" = "http://middlesexconservation.org/wp-content/uploads/2012/02/irissetosacanadensis.gif",
